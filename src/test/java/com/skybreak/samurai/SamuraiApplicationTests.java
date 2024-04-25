ee299c58791b4d38cc51021563277fab28e36518
Ticketedmoon <shane.creedon3@gmail.com>
2024-04-25 12:27:57 +0100

    main: setup application

---
+++ src/test/java/com/skybreak/samurai/SamuraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skybreak.samurai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamuraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
