cf1123c0a2ae13269a7940ed9a2622e6d269374d
Ticketedmoon <shane.creedon3@gmail.com>
2024-05-29 20:04:19 +0100

    main: fix AppTest.java
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- metadata-indexer-service/src/test/java/com/skybreak/samurai/AppTest.java
+++ metadata-indexer-service/src/test/java/com/skybreak/samurai/AppTest.java
@@ -1,13 +1,15 @@
 package com.skybreak.samurai;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
     /**
      * Rigorous Test :-)
      */
