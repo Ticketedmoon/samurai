3cc120d2a2dc148996977724f6360db7ef9a8766
Ticketedmoon <shane.creedon3@gmail.com>
2024-06-02 14:58:44 +0100

    main: transform change events to use highest resolution screenshot urls
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- metadata-indexer-service/src/main/java/com/skybreak/samurai/application/service/GameIndexingOrchestrator.java
+++ metadata-indexer-service/src/main/java/com/skybreak/samurai/application/service/GameIndexingOrchestrator.java
@@ -2,9 +2,11 @@ package com.skybreak.samurai.application.service;
 
 import com.skybreak.samurai.application.domain.dto.SamuraiDocument;
 import com.skybreak.samurai.infrastructure.domain.model.GameChangeEvent;
+import com.skybreak.samurai.infrastructure.domain.model.GameScreenshot;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +14,37 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GameIndexingOrchestrator {
 
+    private static final String IGDB_HIGHEST_RESOLUTION_TYPE = "t_1080p";
+
     private final IndexingService indexingService;
 
     public void performChangeEventIndexing(GameChangeEvent event) throws IOException {
         BigDecimal rating = calculateRating(event);
+        List<GameScreenshot> screenshots = buildHighResolutionScreenshot(event);
         SamuraiDocument document = SamuraiDocument.builder()
                 .id(event.getKey())
                 .name(event.getName())
                 .summary(event.getSummary())
                 .samuraiRating(rating)
-                .screenshots(event.getScreenshots())
+                .screenshots(screenshots)
                 .build();
 
         indexingService.performIndex(document);
+    }
+
+    private static List<GameScreenshot> buildHighResolutionScreenshot(GameChangeEvent event) {
+        return event.getScreenshots()
+                .stream()
+                .map(gameScreenshot -> {
+                    String[] groups = gameScreenshot.getUrl().split("/");
+                    groups[groups.length - 2] = IGDB_HIGHEST_RESOLUTION_TYPE;
+                    String newUrl = String.join("/", groups);
+                    return GameScreenshot.builder()
+                            .id(gameScreenshot.getId())
+                            .url(newUrl)
+                            .build();
+                })
+                .toList();
     }
 
     private static BigDecimal calculateRating(GameChangeEvent event) {
