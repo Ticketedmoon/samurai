a0ac0cf6b6c4cd8dbfdf5e3c38fd308902efdeee
Ticketedmoon <shane.creedon3@gmail.com>
2024-06-29 21:09:01 +0100

    main: Add comment describing polling solution and how we would handle instance restarts and cursor position loss
    Signed-off-by: Ticketedmoon <shane.creedon3@gmail.com>

--- metadata-aggregation-service/src/main/java/com/skybreak/samurai/application/service/GameCollectionService.java
+++ metadata-aggregation-service/src/main/java/com/skybreak/samurai/application/service/GameCollectionService.java
@@ -26,17 +26,27 @@ import proto.GameResult;
 @RequiredArgsConstructor
 public class GameCollectionService {
 
+    private static final int TOTAL_GAMES_TO_CONSUME_PER_PAGE = 100;
     private static final String NEXT_GAME_LIST_API_QUERY_TEMPLATE = """
         fields: id, name, summary, rating, aggregated_rating, screenshots.url;
         sort aggregated_rating desc;
         offset %s;
-        limit 100;
+                limit %s;
         """;
 
     private final ApiClientProperties apiClientProperties;
 
     private final GameKafkaProducer gameKafkaProducer;
 
+    // When changing this to @Scheduled to poll documents with a certain periodicity, we do not want to lose the
+    // position or cursor of the catalogue scan. Therefore, in fear of instance or pod restarts, we want to persist the
+    // scan position periodically, so we never need to restart the scan fully. We may lose a small bit of progress but
+    // that is trivial, and a trade-off that I am fine to allow.
+    // To achieve this, we could write to disk periodically and then once the application comes back online,
+    // restart from the persisted position. This works with 1 instance, but how does this interplay with several?
+    // Rather than use disk, I think a config sever would be good for reading and writing the position of where the scan
+    // is. This needs more thought, but we can consider synchronization mechanisms between instances to not scan the
+    // same section of the game catalogue twice.
     @PostConstruct
     void scanGameCatalogueForEvents() throws InvalidProtocolBufferException, RequestException {
         TwitchToken twitchToken = acquireAccessToken();
@@ -81,7 +91,7 @@ public class GameCollectionService {
     }
 
     private List<Game> searchGames(IGDBWrapper wrapper, long positionOffset) throws RequestException, InvalidProtocolBufferException {
-        String query = NEXT_GAME_LIST_API_QUERY_TEMPLATE.formatted(positionOffset);
+        String query = NEXT_GAME_LIST_API_QUERY_TEMPLATE.formatted(positionOffset, TOTAL_GAMES_TO_CONSUME_PER_PAGE);
         byte[] bytes = wrapper.apiProtoRequest(Endpoints.GAMES, query);
         return GameResult.parseFrom(bytes).getGamesList();
     }
